009748d4250c1040a7d3137f77f3250972723769
Arran Ubels <arran4@gmail.com>
2026-03-18 20:54:39 +1100

    Removed file

--- fix_engine.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open("src/core/Engine.cpp", "r") as f:
-    data = f.read()
-
-# I need to fix the logic of existing loading
-# The user wants to: "add a version string, such that we load the latest version, the 'development' version is considered later if everything else is the same."
-# And I broke some things and missed the `QJsonObject` include maybe?
